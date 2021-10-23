a3321eadac264db75b3b496183d92cad6cf48bce
abdlrahman1 <92398027+abdlrahman1@users.noreply.github.com>
2021-10-24 01:25:29 +0200

    Add files via upload

---
+++ search/search.php
@@ -0,0 +1,261 @@
+<html>
+<head>
+
+   <meta charset = "utf-8">
+     <meta name = "description" content = "welcome to my website">
+     <meta name = "viewport" content = "width=device-width,intial-scale=1.0">
+	 <link rel="stylesheet" href="css/search.css">
+	    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	 
+
+
+</head>
+
+
+
+<body>
+
+<?php include('header.php'); ?>
+<div class="Conall">
+<div class=" blog-form">
+					<h2 class="blog-sidebar-title"><b>gategory</b></h2>
+					
+					fashon
+					<hr />
+					<p class="blog-sidebar-text">Brand</p>
+				     <input type="checkbox"> Generic </input>
+					<div>&nbsp;</div>
+
+					<h2 class="blog-sidebar-title"><b>Size</b></h2>
+					<hr />
+					<p class="blog-sidebar-list"><input type="checkbox">  </span> S</p>
+					<p class="blog-sidebar-list"><input type="checkbox">  </span> M</p>
+					<p class="blog-sidebar-list"><input type="checkbox">  </span> L</p>
+					<p class="blog-sidebar-list"><input type="checkbox">  </span> XL</p>
+					<p class="blog-sidebar-list"><input type="checkbox">  </span> 2XL</p>
+                    <p class="blog-sidebar-list"><input type="checkbox">  </span> 3XL</p>
+					<p class="blog-sidebar-list"><input type="checkbox">  </span> 4XL</p>
+					<p class="blog-sidebar-list"><input type="checkbox">  </span> 5XL</p>
+					<hr>
+				
+					
+
+					<h4 class="blog-sidebar-title">PRICE (EGP) </h4>
+						<a href="#" class="upto"> up to 25$</a>
+						<br>
+						<br>
+						<a href="#" class="upto"> 25$ to 50$</a>
+						<br>
+						<br>
+						<a href="#" class="upto"> 50$ to 100$</a>
+						<br>
+						<br>
+						<a href="#"class="upto"> 100$ to 200$</a>
+						<br>
+						<br>
+						<a href="#" class="upto"> 200$ & above</a>
+						<br>
+						<Br>
+						<input type="text" placeholder="$Max" class="min-max">
+						
+					<input type="text" placeholder="$Min" class="min-max">
+					<button type="button" class="btn"> go </button>
+				
+					
+					
+					<hr />
+                 <h4> Brand </h4>
+				<p class="blog-sidebar-list"><input type="checkbox">  </span> LAOLASI</p>
+				<p class="blog-sidebar-list"><input type="checkbox">  </span> REORIA</p>
+				<p class="blog-sidebar-list"><input type="checkbox">  </span> Nautica</p>
+				<p class="blog-sidebar-list"><input type="checkbox">  </span> Lacoste</p>
+				<hr>
+<div>
+				<P> Customer Reviews </p>
+				<a href="#" class="star">
+				 <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"id="raiting"></span> &up
+			  </a>
+</div>
+			  <a href="#" class="star">
+			  <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star" id="raiting"></span>
+              <span class="fa fa-star"id="raiting"></span> &up
+</a>
+			  <div>
+			  <a href="#" class="star">
+			  <span class="fa fa-star"></span>
+              <span class="fa fa-star"></span>
+              <span class="fa fa-star" id="raiting"></span>
+              <span class="fa fa-star" id="raiting"></span>
+              <span class="fa fa-star"id="raiting"></span> &up
+</a>
+			  
+			  
+			  </div>
+			  
+			  
+			  
+			  		  <div>
+			  <a href="#" class="star">
+			  <span class="fa fa-star"></span>
+              <span class="fa fa-star"id="raiting"></span>
+              <span class="fa fa-star" id="raiting"></span>
+              <span class="fa fa-star" id="raiting"></span>
+              <span class="fa fa-star"id="raiting"></span> &up
+</a>
+			  
+			  
+			  </div>
+			  <hr>
+			  <div>
+		<a href="#"style="text-decoration:none";>	<input type="checkbox">  shipped from estore </a> </input> <br>
+		<a href="#"style="text-decoration:none";>	<input type="checkbox">  shipped from seller </a> </input>
+			  </div>
+			  
+			  <div>
+			  <hr>
+			  <h4>Availability </h4>
+			  <a href="#"style="text-decoration:none";>	<input type="checkbox"> include out of stock  </a> </input> <br>
+			  
+			  </div>
+			 
+			  
+			  
+			  
+				</div>
+				
+				<div class="con"> 
+				
+				<h2> filter department </h2>
+				
+				<button type="button" class="btn-department"> Men </button>
+				<button type="button" class="btn-department"> women </button>
+			
+				<button type="button" class="btn-department"> Childern </button>
+				
+			 <div class="select">
+							<select class="form-control">
+								<option value="">Default Sorting</option>
+								<option value="popularity">Sorting by popularity</option>
+								<option value="average">Sorting by average</option>
+								<option value="latest">Sorting by latest</option>
+								<option value="low">Sorting by low</option>
+								<option value="high">Sorting by high</option>
+							</select>
+							
+						</div>
+						<hr>
+				<p style="color:#75757a; font-size:15px">  8 products found    </p> 
+				
+		
+				
+			
+				
+			
+			
+			
+	
+			
+			</div>
+			
+			
+			
+				
+			
+			 <div class="container">
+			 <hr>
+			 
+			 
+			 
+       
+        <div class="products">
+          <img src="img/prod4.jpg" alt="prod">
+          <span>item1</span>
+          <h3>EGP 600</h3>
+		   <h4>EGP 400</h4>
+         
+        </div>
+        <div class="products">
+          <img src="img/prod3.jpg" alt="prod">
+          <span>item1</span>
+          <h3>EGP 600</h3>
+		   <h4>EGP 400</h4>
+     
+        </div>
+        <div class="products">
+          <img src="img/prod1.jpg" alt="prod">
+          <span>item1</span>
+          <h3>EGP 600</h3>
+		   <h4>EGP 400</h4>
+      
+        </div>
+        <div class="products">
+          <img src="img/prod2.jpg" alt="prod">
+          <span>item1</span>
+          <h3>EGP 600</h3>
+		   <h4>EGP 400</h4>
+          
+        </div>
+        
+			
+			
+				
+			
+				
+			</div>
+			
+			
+			
+						 <div class="c">
+        <h2>top selling </h2>
+		<br>
+		<br>
+        <div class="pro">
+		
+          <img src="img/prod4.jpg" alt="prod">
+          <span>item1</span>
+          <h3>EGP 600</h3>
+           <h4>EGP 400</h4>
+        </div>
+        <div class="pro">
+          <img src="img/prod3.jpg" alt="prod">
+          <span>item1</span>
+          <h3>EGP 600</h3>
+         <h4>EGP 400</h4>
+        </div>
+        <div class="pro">
+          <img src="img/prod2.jpg" alt="prod">
+          <span>item1</span>
+          <h3>EGP 600</h3>
+		   <h4>EGP 400</h4>
+       
+        </div>
+        <div class="pro">
+          <img src="img/prod4.jpg" alt="prod">
+          <span>item1</span>
+          <h3>EGP 600</h3>
+		   <h4>EGP 400</h4>
+
+        </div>
+        <div class="pro">
+          <img src="img/prod4.jpg" alt="prod">
+          <span>item1</span>
+          <h3>EGP 600</h3>
+		   <h4>EGP 400</h4>
+
+        </div>
+
+		
+        </div>
+		</div>
+       <?php include('footer.php'); ?>
+</body>
+
+		
